49ff9d14935adbbce70618b5f61856963b2083b4
Mark Kolbanev <onecupoftea93@gmail.com>
2023-04-03 22:55:25 +0300

    Social svgs turned into components

---
+++ src/components/Social/SocialButtons/index.js
@@ -0,0 +1,6 @@
+import { FacebookButton } from './FacebookButton.js';
+import { InstagramButton } from './InstagramButton.js';
+import { TwitterButton } from './TwitterButton.js';
+import { YoutubeButton } from './YoutubeButton.js';
+
+export {FacebookButton,InstagramButton,TwitterButton,YoutubeButton};
